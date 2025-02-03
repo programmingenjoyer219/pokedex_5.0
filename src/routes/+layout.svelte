5301759e68e1456f197ce256f19c32dc63d7c584
Yuganshu <yuganshu219@gmail.com>
2025-02-03 16:34:33 +0530

    setup app styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import "$styles/reset.css";
+  import "$styles/normalize.css";
+  import "$styles/app.css";
+
+  var { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+{@render children()}
