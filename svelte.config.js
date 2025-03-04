2b1f324f5b32730e5786e3e8e97735657b96fafd
Yuganshu <yuganshu219@gmail.com>
2025-03-04 23:05:20 +0530

    bug fix: update svelte.config.js to prerender 898 pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,27 @@
 import adapter from "@sveltejs/adapter-vercel";
 
+function generateEntries() {
+    var result = [];
+    for (let i = 1; i <= 898; i += 1) {
+        result.push(`/pokemon/${i}`);
+    }
+    return result;
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $styles: "./src/styles",
-      $components: "./src/components",
-      $features: "./src/features",
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $styles: "./src/styles",
+            $components: "./src/components",
+            $features: "./src/features",
+        },
+        prerender: {
+            crawl: true,
+            entries: ["*", ...generateEntries()],
+        },
     },
-  },
 };
 
 export default config;
