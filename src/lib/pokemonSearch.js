fa8005d9f0e36d267de0063e47f49e8f46ca1db0
Yuganshu <yuganshu219@gmail.com>
2025-03-09 12:40:20 +0530

    abstract filter checks to their own functions, use regex to match pokemons by their name

--- src/lib/pokemonSearch.js
+++ src/lib/pokemonSearch.js
@@ -1,10 +1,25 @@
 export function provideFilterCriteria(pokemonType, searchQuery) {
-    return function filterCriteria(p) {
-        return pokemonType
-            ? p.type.includes(pokemonType) &&
-                  p.name.english
-                      .toLowerCase()
-                      .includes(searchQuery.toLowerCase())
-            : p.name.english.toLowerCase().includes(searchQuery.toLowerCase());
+    return function filterCriteria(currentPokemon) {
+        var doesNameMatch = matchPokemonByName(
+            searchQuery,
+            currentPokemon.name.english
+        );
+        if (pokemonType) {
+            return (
+                matchPokemonByType(pokemonType, currentPokemon.type) &&
+                doesNameMatch
+            );
+        } else {
+            return doesNameMatch;
+        }
     };
+}
+
+function matchPokemonByType(typeSearchQuery, currentPokemonType) {
+    return currentPokemonType.includes(typeSearchQuery);
+}
+
+function matchPokemonByName(nameSearchQuery, currentPokemonName) {
+    var regex = new RegExp(nameSearchQuery, "i");
+    return regex.test(currentPokemonName);
 }
